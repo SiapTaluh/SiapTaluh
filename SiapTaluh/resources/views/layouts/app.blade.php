d527afed69c57659fa463916cc545d9dff5af4bf
I Ketut Andika Wisnu Danuarta <60722042+Waizsec@users.noreply.github.com>
2021-01-15 12:45:01 +0800

    Homepage
    Masih kurang footer sama deskripsi..

---
+++ SiapTaluh/resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>
+      @yield('title')
+    </title>
+    <link rel="shortcut icon" type="image/png" href="favicon.png">
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+{{-- header Part --}}
+
+@yield('header')
+    
+{{-- Ends header Part --}}
+
+{{-- Body Part --}}
+
+    @yield('body')
+    
+{{-- Ends Body Part --}}
+  </body>
+</html>
